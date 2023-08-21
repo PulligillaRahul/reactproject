c5a276aa763ae75f9f897caaca3db02a47a6b8ab
P Rahul <p.rahul@globallogic.com>
2023-08-21 21:12:46 +0530

    Headers and Footers

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; 2023 Login Application</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
